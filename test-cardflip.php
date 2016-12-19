6b8eec79d4260c5b88005ef75bc7fd2d62524bad
Michelle Kirby <Michelle Kirby>
2016-12-19 15:54:27 -0500

    Click cards close, no more rotate initiated

--- test-cardflip.php
+++ test-cardflip.php
@@ -1,7 +1,7 @@
 <?php include "header.php" ?>
 
 <ul class="cards">
-	<button class="cards-close">x <span>close</span></button>
+	<li><button class="cards-close">x <span>close</span></button></li>
 	<li id="Card-pink" class="card"></li>
 	<li id="Card-red" class="card"></li>
 	<li id="Card-orange" class="card"></li>
