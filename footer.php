29458904df926ccde9bb45f2f0a0d90ecdba06cf
lilgorilla <mshhll.rk@gmail.com>
2017-01-04 17:07:25 -0500

    Arranged flags in a circle, Included svgs and names. To Do: Rotate flags and names according to position

--- footer.php
+++ footer.php
@@ -5,4 +5,6 @@
 	    <!--  Scipts / Custom -->
         <script src="lib/js/main.js"></script>
         <script src="lib/js/test-cardflip.js"></script>
+        <script src="lib/js/vendor/lettering.js"></script>
+        <script src="lib/js/vendor/circletype.min.js"></script>
 </body>
--- map.php
+++ map.php
@@ -6,24 +6,95 @@
 	<img class="map_image" src="lib/img/rotatingMap.png" alt="Rotating map"/>
 </div>
 
-<div id="Flags">
-	<div id="Flag-Canada" class="flag" data-target="#FlagContent-Canada">
-		<img src="lib/svg/flags/canada.svg" alt="Canadian Flag"/>
-	</div>
-</div>
+<ul id="Flags">
+	<li id="Flag-Tonga" class="flag">
+		<p>Tonga</p>
+		<img src="lib/svg/flags/tonga.svg" alt="Tonga Flag" />
+	</li>
+	<li id="Flag-CookIslands" class="flag">
+		<p>Cook Islands</p>
+		<img src="lib/svg/flags/cookislands.svg" alt="Cook Islands Flag" />
+	</li>
+	<li id="Flag-Canada" class="flag">
+		<p>Canada</p>
+		<img src="lib/svg/flags/canada.svg" alt="Tonga Flag" />
+	</li>
+	<li id="Flag-USA" class="flag">
+		<p>U.S.A</p>
+	</li>
+	<li id="Flag-DominicanRepublic" class="flag">
+		<p>Dominican Republic</p>
+		<img src="lib/svg/flags/dominicanrepublic.svg" alt="Dominican Republic Flag" />
+	</li>
+	<li id="Flag-Honduras" class="flag">
+		<p>Honduras</p>
+		<img src="lib/svg/flags/honduras.svg" alt="Honduras Flag" />
+	</li>
+	<li id="Flag-Guatemala" class="flag">
+		<p>Guatemala</p>
+		<img src="lib/svg/flags/guatemala.svg" alt="Guatemala Flag" />
+	</li>
+	<li id="Flag-Brazil" class="flag">
+		<p>Brazil</p>
+		<img src="lib/svg/flags/brazil.svg" alt="Brazil Flag" />
+	</li>
+	<li id="Flag-UK" class="flag">
+		<p>United Kingdom</p>
+		<img src="lib/svg/flags/unitedkingdom.svg" alt="United Kingdom Flag" />
+	</li>
+	<li id="Flag-Ireland" class="flag">
+		<p>Ireland</p>
+		<img src="lib/svg/flags/ireland.svg" alt="Ireland Flag" />
+	</li>
+	<li id="Flag-Denmark" class="flag">
+		<p>DenmarK</p>
+		<img src="lib/svg/flags/denmark.svg" alt="Denmark Flag" />
+	</li>
+	<li id="Flag-Sweden" class="flag">
+		<p>Sweden</p>
+		<img src="lib/svg/flags/sweden.svg" alt="Sweden Flag" />
+	</li>
+	<li id="Flag-Israel" class="flag">
+		<p>Israel</p>
+		<img src="lib/svg/flags/israel.svg" alt="Israel Flag" />
+	</li>
+	<li id="Flag-SouthAfrica" class="flag">
+		<p>South Africa</p>
+		<img src="lib/svg/flags/southafrica.svg" alt="South Africa Flag" />
+	</li>
+	<li id="Flag-Malawi" class="flag">
+		<p>Malawi</p>
+		<img src="lib/svg/flags/malawi.svg" alt="Malawi Flag" />
+	</li>
+	<li id="Flag-Uganda" class="flag">
+		<p>Uganda</p>
+		<img src="lib/svg/flags/uganda.svg" alt="Uganda Flag" />
+	</li>
+	<li id="Flag-Ethiopia" class="flag">
+		<p>Ethiopia</p>
+		<img src="lib/svg/flags/ethiopia.svg" alt="Ethiopia Flag" />
+	</li>
+	<li id="Flag-Russia" class="flag">
+		<p>Russia</p>
+		<img src="lib/svg/flags/russia.svg" alt="Russia Flag" />
+	</li>
+	<li id="Flag-SouthKorea" class="flag">
+		<p>South Korea</p>
+		<img src="lib/svg/flags/southkorea.svg" alt="South Korea Flag" />
+	</li>
+	<li id="Flag-Phillippines" class="flag">
+		<p>The Phillippines</p>
+		<img src="lib/svg/flags/phillippines.svg" alt="The Phillippines Flag" />
+	</li>
+	<li id="Flag-Australia" class="flag">
+		<p>Australia</p>
+		<img src="lib/svg/flags/australia.svg" alt="Australia Flag" />
+	</li>
+	<li id="Flag-Fiji" class="flag">
+		<p>Fiji</p>
+		<img src="lib/svg/flags/fiji.svg" alt="Fiji Flag" />
+	</li>
+</ul>
 
-<div id="FlagsContent">
-	<div id="FlagContent-Canada" class="flagcontent">
-		<button class="flagcontent-close">x <span>close</span></button>
-		<ul>
-			<li class="flagcontent-description">
-				<div class="flagcontent-description-border">
-					<h1 class="flagcontent-description-title">Canada</h1>
-					<p class="flagcontent-description-text">Pommy ipsum football a diamond geezer, oo ecky thump. Bloody shambles god save the queen Dr. Watson nonsense god save the queen Weeping Angels lad, cheesed off in the jacksy on his bill supper nowt bowler hat Dr. Watson, barmy for sooth Doctor Who see a man about a dog rubbish. Narky fancy a cuppa bit of alright one feels that bobby naff, jolly good made a pig's ear of it macca Dr. Watson, grub's up in the jacksy chippy Weeping Angels. Picalilly clock round the earhole alright duck off with her head some mothers do 'ave 'em Amelia Pond rumpy pumpy, pants one off The Doctor cobbles manky.</p>
-				</div>
-			</li>
-		</ul>
-	</div>
-</div>
 
 <?php include "footer.php" ?>
