64e4e873bdaad84a3a308aec0c21f11edb937074
Michelle Kirby <Michelle Kirby>
2017-01-05 10:58:57 -0500

    Removed flag labels to be replaced by svgs

--- map.php
+++ map.php
@@ -8,90 +8,68 @@
 
 <ul id="Flags">
 	<li id="Flag-Tonga" class="flag">
-		<p>Tonga</p>
 		<img src="lib/svg/flags/tonga.svg" alt="Tonga Flag" />
 	</li>
 	<li id="Flag-CookIslands" class="flag">
-		<p>Cook Islands</p>
 		<img src="lib/svg/flags/cookislands.svg" alt="Cook Islands Flag" />
 	</li>
 	<li id="Flag-Canada" class="flag">
-		<p>Canada</p>
 		<img src="lib/svg/flags/canada.svg" alt="Tonga Flag" />
 	</li>
 	<li id="Flag-USA" class="flag">
-		<p>U.S.A</p>
 	</li>
 	<li id="Flag-DominicanRepublic" class="flag">
-		<p>Dominican Republic</p>
 		<img src="lib/svg/flags/dominicanrepublic.svg" alt="Dominican Republic Flag" />
 	</li>
 	<li id="Flag-Honduras" class="flag">
-		<p>Honduras</p>
 		<img src="lib/svg/flags/honduras.svg" alt="Honduras Flag" />
 	</li>
 	<li id="Flag-Guatemala" class="flag">
-		<p>Guatemala</p>
 		<img src="lib/svg/flags/guatemala.svg" alt="Guatemala Flag" />
 	</li>
 	<li id="Flag-Brazil" class="flag">
-		<p>Brazil</p>
 		<img src="lib/svg/flags/brazil.svg" alt="Brazil Flag" />
 	</li>
 	<li id="Flag-UK" class="flag">
-		<p>United Kingdom</p>
 		<img src="lib/svg/flags/unitedkingdom.svg" alt="United Kingdom Flag" />
 	</li>
 	<li id="Flag-Ireland" class="flag">
-		<p>Ireland</p>
 		<img src="lib/svg/flags/ireland.svg" alt="Ireland Flag" />
 	</li>
 	<li id="Flag-Denmark" class="flag">
-		<p>DenmarK</p>
 		<img src="lib/svg/flags/denmark.svg" alt="Denmark Flag" />
 	</li>
 	<li id="Flag-Sweden" class="flag">
-		<p>Sweden</p>
 		<img src="lib/svg/flags/sweden.svg" alt="Sweden Flag" />
 	</li>
 	<li id="Flag-Israel" class="flag">
-		<p>Israel</p>
 		<img src="lib/svg/flags/israel.svg" alt="Israel Flag" />
 	</li>
 	<li id="Flag-SouthAfrica" class="flag">
-		<p>South Africa</p>
 		<img src="lib/svg/flags/southafrica.svg" alt="South Africa Flag" />
 	</li>
 	<li id="Flag-Malawi" class="flag">
-		<p>Malawi</p>
 		<img src="lib/svg/flags/malawi.svg" alt="Malawi Flag" />
 	</li>
 	<li id="Flag-Uganda" class="flag">
-		<p>Uganda</p>
 		<img src="lib/svg/flags/uganda.svg" alt="Uganda Flag" />
 	</li>
 	<li id="Flag-Ethiopia" class="flag">
-		<p>Ethiopia</p>
 		<img src="lib/svg/flags/ethiopia.svg" alt="Ethiopia Flag" />
 	</li>
 	<li id="Flag-Russia" class="flag">
-		<p>Russia</p>
 		<img src="lib/svg/flags/russia.svg" alt="Russia Flag" />
 	</li>
 	<li id="Flag-SouthKorea" class="flag">
-		<p>South Korea</p>
 		<img src="lib/svg/flags/southkorea.svg" alt="South Korea Flag" />
 	</li>
 	<li id="Flag-Phillippines" class="flag">
-		<p>The Phillippines</p>
 		<img src="lib/svg/flags/phillippines.svg" alt="The Phillippines Flag" />
 	</li>
 	<li id="Flag-Australia" class="flag">
-		<p>Australia</p>
 		<img src="lib/svg/flags/australia.svg" alt="Australia Flag" />
 	</li>
 	<li id="Flag-Fiji" class="flag">
-		<p>Fiji</p>
 		<img src="lib/svg/flags/fiji.svg" alt="Fiji Flag" />
 	</li>
 </ul>
