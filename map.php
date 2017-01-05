6521626d785624cad7541a881d38410ba735eb20
Michelle Kirby <Michelle Kirby>
2017-01-05 16:05:35 -0500

    Flag trigger to open placeholder card stack oriented on the polar coordinates

--- map.php
+++ map.php
@@ -6,72 +6,89 @@
 	<img class="map_image" src="lib/img/rotatingMap.png" alt="Rotating map"/>
 </div>
 
+<div id="Flag-Labels">
+	<img src="lib/svg/country_names.svg"/>
+</div>
+
 <ul id="Flags">
-	<li id="Flag-Tonga" class="flag">
-		<img src="lib/svg/flags/tonga.svg" alt="Tonga Flag" />
-	</li>
-	<li id="Flag-CookIslands" class="flag">
-		<img src="lib/svg/flags/cookislands.svg" alt="Cook Islands Flag" />
-	</li>
-	<li id="Flag-Canada" class="flag">
-		<img src="lib/svg/flags/canada.svg" alt="Tonga Flag" />
-	</li>
-	<li id="Flag-USA" class="flag">
-	</li>
-	<li id="Flag-DominicanRepublic" class="flag">
-		<img src="lib/svg/flags/dominicanrepublic.svg" alt="Dominican Republic Flag" />
-	</li>
-	<li id="Flag-Honduras" class="flag">
-		<img src="lib/svg/flags/honduras.svg" alt="Honduras Flag" />
-	</li>
-	<li id="Flag-Guatemala" class="flag">
-		<img src="lib/svg/flags/guatemala.svg" alt="Guatemala Flag" />
-	</li>
-	<li id="Flag-Brazil" class="flag">
-		<img src="lib/svg/flags/brazil.svg" alt="Brazil Flag" />
-	</li>
-	<li id="Flag-UK" class="flag">
-		<img src="lib/svg/flags/unitedkingdom.svg" alt="United Kingdom Flag" />
-	</li>
-	<li id="Flag-Ireland" class="flag">
-		<img src="lib/svg/flags/ireland.svg" alt="Ireland Flag" />
-	</li>
-	<li id="Flag-Denmark" class="flag">
-		<img src="lib/svg/flags/denmark.svg" alt="Denmark Flag" />
-	</li>
-	<li id="Flag-Sweden" class="flag">
-		<img src="lib/svg/flags/sweden.svg" alt="Sweden Flag" />
-	</li>
-	<li id="Flag-Israel" class="flag">
-		<img src="lib/svg/flags/israel.svg" alt="Israel Flag" />
-	</li>
-	<li id="Flag-SouthAfrica" class="flag">
-		<img src="lib/svg/flags/southafrica.svg" alt="South Africa Flag" />
-	</li>
-	<li id="Flag-Malawi" class="flag">
-		<img src="lib/svg/flags/malawi.svg" alt="Malawi Flag" />
-	</li>
-	<li id="Flag-Uganda" class="flag">
-		<img src="lib/svg/flags/uganda.svg" alt="Uganda Flag" />
-	</li>
-	<li id="Flag-Ethiopia" class="flag">
+	<li id="Flag-Ethiopia" class="flag" data-targetcards="#TestCards">
 		<img src="lib/svg/flags/ethiopia.svg" alt="Ethiopia Flag" />
 	</li>
-	<li id="Flag-Russia" class="flag">
-		<img src="lib/svg/flags/russia.svg" alt="Russia Flag" />
+	<li id="Flag-Uganda" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/uganda.svg" alt="Uganda Flag" />
 	</li>
-	<li id="Flag-SouthKorea" class="flag">
-		<img src="lib/svg/flags/southkorea.svg" alt="South Korea Flag" />
+	<li id="Flag-Malawi" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/malawi.svg" alt="Malawi Flag" />
 	</li>
-	<li id="Flag-Phillippines" class="flag">
-		<img src="lib/svg/flags/phillippines.svg" alt="The Phillippines Flag" />
+	<li id="Flag-SouthAfrica" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/southafrica.svg" alt="South Africa Flag" />
 	</li>
-	<li id="Flag-Australia" class="flag">
-		<img src="lib/svg/flags/australia.svg" alt="Australia Flag" />
+	<li id="Flag-Israel" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/israel.svg" alt="Israel Flag" />
 	</li>
-	<li id="Flag-Fiji" class="flag">
+	<li id="Flag-Sweden" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/sweden.svg" alt="Sweden Flag" />
+	</li>
+	<li id="Flag-Denmark" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/denmark.svg" alt="Denmark Flag" />
+	</li>
+	<li id="Flag-UK" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/unitedkingdom.svg" alt="United Kingdom Flag" />
+	</li>
+	<li id="Flag-Ireland" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/ireland.svg" alt="Ireland Flag" />
+	</li>
+	<li id="Flag-Brazil" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/brazil.svg" alt="Brazil Flag" />
+	</li>
+	<li id="Flag-DominicanRepublic" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/dominicanrepublic.svg" alt="Dominican Republic Flag" />
+	</li>
+	<li id="Flag-Guatemala" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/guatemala.svg" alt="Guatemala Flag" />
+	</li>
+	<li id="Flag-Honduras" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/honduras.svg" alt="Honduras Flag" />
+	</li>
+	
+	<li id="Flag-USA" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/canada.svg" alt="Tonga Flag" />
+	</li>
+	<li id="Flag-Canada" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/canada.svg" alt="Tonga Flag" />
+	</li>
+	<li id="Flag-CookIslands" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/cookislands.svg" alt="Cook Islands Flag" />
+	</li>
+	<li id="Flag-Tonga" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/tonga.svg" alt="Tonga Flag" />
+	</li>
+	<li id="Flag-Fiji" class="flag" data-targetcards="#TestCards">
 		<img src="lib/svg/flags/fiji.svg" alt="Fiji Flag" />
 	</li>
+	<li id="Flag-Australia" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/australia.svg" alt="Australia Flag" />
+	</li>
+	<li id="Flag-Phillippines" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/phillippines.svg" alt="The Phillippines Flag" />
+	</li>
+	<li id="Flag-SouthKorea" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/southkorea.svg" alt="South Korea Flag" />
+	</li>
+	<li id="Flag-Russia" class="flag" data-targetcards="#TestCards">
+		<img src="lib/svg/flags/russia.svg" alt="Russia Flag" />
+	</li>
+</ul>
+
+<ul id="TestCards" class="cards">
+	<li><button class="cards-close">x <span>close</span></button></li>
+	<li id="Card-pink" class="card"></li>
+	<li id="Card-red" class="card"></li>
+	<li id="Card-orange" class="card"></li>
+	<li id="Card-yellow" class="card"></li>
+	<li id="Card-green" class="card"></li>
+	<li id="Card-blue" class="card"></li>
+	<li id="Card-purple" class="card"></li>
 </ul>
 
 
