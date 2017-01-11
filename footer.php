07afa61d6a90ed01405803e1d445101365903ae2
Michelle Kirby <Michelle Kirby>
2017-01-11 18:10:15 -0500

    Edge test complete. Using jquery's native animate function. Need to clean up code

--- footer.php
+++ footer.php
@@ -3,7 +3,6 @@
 	</footer>
 
 	    <!--  Scipts / Custom -->
-	    <script src="lib/js/vendor/transit.min.js"></script>
         <script src="lib/js/main.js"></script>
         <script src="lib/js/test-cardflip.js"></script>
         <script src="lib/js/vendor/lettering.js"></script>
